be1db0b2ad55be6d9dffb408cde0f4eab84863e1
jke <jke@zuehlke.com>
2015-10-12 15:40:36 +0200

    first failing test

---
+++ src/test/java/com/zuehlke/math/test/EnvironmentTest.java
@@ -0,0 +1,12 @@
+package com.zuehlke.math.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EnvironmentTest {
+    
+    @Test
+    public void failing() throws Exception {
+        Assert.fail("as requested");
+    }
+}
